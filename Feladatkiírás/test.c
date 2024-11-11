6d98bf5946f01fb84f6e26fb8a1b386e5c79c548
HBalassa <hodi.balassa@gmail.com>
2024-11-11 12:34:30 +0100

    decimal_to_binary conversion. 11-11

--- Feladatkiírás/test.c
+++ Feladatkiírás/test.c
@@ -9,28 +9,35 @@ uint8_t decimal_to_bcd(uint16_t);
 void main(void) {
   uint8_t num = (DS1307_ADR<<1) | (1<<0);
 
+  // check DS1307_ADR
   for(uint8_t i=0; i<8; i++) {
     if((num & (1<<(7-i))) == 0) printf("0");
     else printf("1");
   }
+
+  printf("\n");
   
-  
-  // decimal_to_bcd(2145);
+  // check decimal to bcd
+  for(uint8_t i=0; i<8; i++) {
+    if(i == 4) {
+      printf(" ");
+    }
+    if((decimal_to_bcd(46) & (1<<(7-i))) == 0) printf("0");
+    else printf("1");
+  }
 }
 
 uint8_t decimal_to_bcd(uint16_t dec_num) {
-  if(dec_num == 0) return 0;
   // to convert from dec to bcd, just simply convert every digit to a 4-bit binary number
-  // intervall: 0...59 | 2000...2100
+  // the input is between 0...99
+  if(dec_num > 99) return -1;
 
-  // examine the length of the number
-  uint8_t length = 0;
-  uint16_t result = dec_num / pow(10, length);
+  // dec_num = second_digit*10 + first_digit
+  uint8_t first_digit = dec_num % 10;
+  uint8_t second_digit = dec_num / 10;
 
-  while(result != 0) {
-    length++;
-    result = dec_num / pow(10, length);
-  }
+  // number in bcd
+  uint8_t bcd_num = (second_digit<<4) | (first_digit);
 
-  // write to bcd
+  return bcd_num;
 }
